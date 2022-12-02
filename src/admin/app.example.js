6e5f2df40b0953f3df374f5cef1974ab68db96b2
OmkarEdz <omkar.t@edreamz.in>
2022-12-02 13:21:25 +0530

    initial commit

---
+++ src/admin/app.example.js
@@ -0,0 +1,43 @@
+export default {
+  config: {
+    auth: {
+      logo:'',
+    },
+    menu:{
+      logo: ''
+    },
+    tutorials: false,
+    notifications: { release: false },
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
